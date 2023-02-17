946d7a020019020834da1fa8ed324c87c00c0b3c
samudraon <samudraon@gmailcom>
2023-02-17 17:29:25 +0600

    disabled button added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let count = 0;
-// let totalPrice = 0;
+let productTotalPrice = 0;
 
 /* for first card */
 document.getElementById('first-card').addEventListener('click', function () {
@@ -10,8 +10,11 @@ document.getElementById('first-card').addEventListener('click', function () {
 
     const priceTotal = parseInt(productPrice) * parseInt(productQuantity);
     count++;
+    productTotalPrice += priceTotal;
+    totalPrice(productTotalPrice);
     /* show the data */
     displayData(productName, productPrice, productQuantity, priceTotal);
+    disabledButton('first-card');
 });
 
 /* for second card */
@@ -22,7 +25,10 @@ document.getElementById('second-card').addEventListener('click', function (e) {
 
     const sumTotal = parseInt(pPrice) + parseInt(pQuantity);
     count++;
+    productTotalPrice += sumTotal;
+    totalPrice(productTotalPrice);
     displayData(pName, pPrice, pQuantity, sumTotal);
+    disabledButton('second-card');
 });
 
 /* third card */
@@ -34,37 +40,33 @@ document.getElementById('third-card').addEventListener('click', function () {
 
     const priceTotal = parseInt(productPrice) - parseInt(productQuantity);
     count++;
+    productTotalPrice += priceTotal;
+    totalPrice(productTotalPrice);
     /* show the data */
     displayData(productName, productPrice, productQuantity, priceTotal);
+    disabledButton('third-card');
 });
+
 /* get fifth card */
 document.getElementById('last-card').addEventListener('click', function () {
     const productName = document.getElementById('last-title').innerText;
     const productPrice = document.getElementById('last-price').value;
     const productQuantity = document.getElementById('last-quantity').value;
-    if (productPrice == '' || productQuantity == '' || productPrice <= 0 || productQuantity <= 0) {
-        return alert('please enter any valid number')
-    }
 
     const priceTotal = parseInt(productPrice) / parseInt(productQuantity);
+
+    if (productPrice == '' || productQuantity == '' || productPrice <= 0 || productQuantity <= 0 || isNaN(productPrice) || isNaN(productQuantity)) {
+        return alert('please enter any valid number');
+    }
     count++;
+    productTotalPrice += priceTotal;
+    totalPrice(productTotalPrice);
     displayData(productName, productPrice, productQuantity, priceTotal);
-})
+    disabledButton('last-card');
+});
+
 
 /* common function */
-function totalPrice() {
-    const total = document.getElementById('total-price');
-}
-
-
-
-
-
-
-
-
-
-
 function displayData(nameOfProduct, priceOfProduct, quantityOfProduct, result) {
     const tableContainer = document.getElementById('table-container');
     const tr = document.createElement('tr');
@@ -76,4 +78,18 @@ function displayData(nameOfProduct, priceOfProduct, quantityOfProduct, result) {
     <td>${result}</td>
     `
     tableContainer.appendChild(tr);
-}
+};
+
+function totalPrice(price) {
+    const totalPrice = document.getElementById('total-price');
+    // const p = document.createElement('p');
+    totalPrice.innerHTML = `
+    <h1 class='text-center font-bold'>Total Price: ${price}</h1>
+    `
+    // totalPrice.innerHTML = p;
+};
+
+/* button disable */
+function disabledButton(id) {
+    document.getElementById(id).setAttribute('disabled', true);
+};
